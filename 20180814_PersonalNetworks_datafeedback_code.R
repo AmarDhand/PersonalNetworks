16cdbb4c8c25eb01d9f6da62b1b8679cab1244b0
LiamMcCafferty <42241726+LiamMcCafferty@users.noreply.github.com>
2020-04-16 17:25:59 -0400

    Graphing Package Fix
    Fixed issue w/ graphing package and incorrect assignment for networks nodes

--- 20180814_PersonalNetworks_datafeedback_code.R
+++ 20180814_PersonalNetworks_datafeedback_code.R
@@ -24,7 +24,7 @@ rm(list=ls())
 #To set to own working directory
 #  select "Session->Set Working Directory->To Source File Location"
 #  then copy result in console into current "setwd("")".
-setwd("~/Desktop/PersonalNetworks-master")
+setwd("~/Desktop/PersNet - Small Networks - Graphfix")
 
 #Importing packages. If not yet installed, packages can be installed by going to:
 #Tools -> Install Packages, then enter their exact names from within each 
@@ -159,18 +159,19 @@ make_net_array <- function(l){
       colors <- c("1" = "blue", "2" = "red")
       
       #Creates unique color palette for "ego" node
-      ego_col <- ifelse(V(ego_g)$name == "EGO", "black", "white")
+      V(ego_g)$ego_col <- ifelse(V(ego_g)$name == "EGO", "black", "white")
       
       #Determines structures of networks
       ggplot(ego_g, aes(x = x, y = y, xend = xend, yend = yend)) + 
         #Determines curvature and thickness of lines (unweighted)
         geom_edges(aes(color = as.factor(weight)), curvature = 0.1) +
         #Determines size and shape of nodes (shape21 = circle)
-        geom_nodes(fill = ego_col, size = 1, shape = 21) +
+        geom_nodes(aes(fill = ego_col), size = 1, shape = 21) +
         #Eliminates labels on each matrix
         theme_blank(legend.position = "none") +
         scale_color_manual(values = colors) +
-        ggtitle(names(matrix_list)[z]) 
+        scale_fill_manual(values = c("black", "white")) +
+        ggtitle(names(matrix_list[z]))
     }
   }
   
@@ -274,11 +275,10 @@ make_image <- function(x) {
   
   #Saves important values for determining graph formatting
   ego4.g  <- graph.adjacency(mat, mode = "undirected", weighted = TRUE)
-  colors  <- c("blue", "red") #create color palette for ties
-  ego_col <- ifelse(V(ego4.g)$name == "You", "grey17", "white") 
+  V(ego4.g)$ego_col <- ifelse(V(ego4.g)$name == "You", "grey17", "white")
   
   #Saves logic to determine the strength of ties between nodes
-  weight.ego <- sapply(E(ego4.g)$weight, function(yk){
+  E(ego4.g)$weight_cat <- sapply(E(ego4.g)$weight, function(yk){
     if(is.na(yk)){
       return("Unknown")
     }else if(yk == 1){
@@ -288,8 +288,7 @@ make_image <- function(x) {
     }
   })
   
-  
-  if ("Unknown" %in% weight.ego ){
+  if ("Unknown" %in% E(ego4.g)$weight_cat ){
     #Error check to see if network has sufficient ties, will output a blank graph with
     #  error message.
     print("Error: Some networks ties are unknown ")
@@ -362,15 +361,14 @@ make_image <- function(x) {
             legend.key.height = unit(1, "line"),
             legend.key = element_rect(colour = NA, fill = NA))
   }else{
-    E(ego4.g)$weight <- weight.ego
     #Creates actual network graph
     plot1 <- ggplot(ego4.g, aes(x = x, y = y, xend = xend, yend = yend, na.rm = FALSE)) + 
       #Determines coloration and style of network edges
-      geom_edges(aes(linetype = as.factor(weight), color = (weight)), curvature = 0.1) +
+      geom_edges(aes(linetype = weight_cat, color = weight_cat), curvature = 0.1) +
       #Fills nodes with ego_color pallate
-      geom_nodes(fill = ego_col, size = 14, shape = 21) +
+      geom_nodes(aes(fill = ego_col), size = 14, shape = 21) +
       #Names each node with alter names
-      geom_nodelabel(label = rownames(mat))+
+      geom_nodelabel(aes(label = name)) +
       theme_blank() +
       #Formats the legend which describes edge weight to the reader
       theme(legend.position = "bottom", #format the legend 
@@ -378,12 +376,14 @@ make_image <- function(x) {
             legend.text = element_text(size = 10)) + 
       theme(legend.title.align = 0.5) + 
       theme(plot.title = element_text(size = 18, face = "bold")) +
-      scale_colour_manual(name = "Tie Strength", values = c("red", "blue"))+
+      scale_colour_manual(name = "Tie Strength", values = c("red", "blue")) +
+      scale_fill_manual(values = c("grey17", "white")) +
       scale_shape_manual(name = "Tie Strength", values = c(22, 21)) +
       scale_linetype_manual(name = "Tie Strength", values = c("solid", "dashed")) +
       #Determins the margins around plot
       theme(plot.margin = unit(c(1.5, 1.5, 1.5, 1.5), "cm")) +
       #Formatting for legend's keys
+      guides(fill = FALSE) +
       theme(legend.direction = 'vertical',
             legend.key.height = unit(1, "line"),
             legend.key = element_rect(colour = NA, fill = NA))
